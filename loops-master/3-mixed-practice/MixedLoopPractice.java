378aed60533b7331731c4b83cb66740e19a5b71b
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-10-05 12:57:44 -0400

    Add files via upload

--- loops-master/3-mixed-practice/MixedLoopPractice.java
+++ loops-master/3-mixed-practice/MixedLoopPractice.java
@@ -116,69 +116,155 @@
     }
     
     //7 uses a while loop to find and return the sum of the first n integers. 
-    public static void sumN(int n) {
+    public static int sumN(int n) {
         int i = 1;
         int sum = 0;
         while (i <= n) {
             sum += i;
             i++;
         }
-        System.out.println(sum);
+        return sum;
     }
     
     //8 uses a for loop to print the first n perfect squares.
-    public static void  perfectSquaresF(int n) {
-    
+    public static void perfectSquaresF(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (Math.sqrt(i) == Math.floor(Math.sqrt(i))) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
     
     //9 uses a while loop find and return the sum of the first n perfect squares
-    public static void sumSquaresW() {
-    
+    public static int sumSquaresW(int n) {
+        int i = 0;
+        int sum = 0;
+        while (i <= n) {
+           if (Math.sqrt(i) == Math.floor(Math.sqrt(i))) {
+                sum += i;
+            } 
+           i++;
+        }
+        return sum;
     }
     
     //10 uses a while loop to print the sentence "I love java." n times.
-    public static void iLoveJava() {
-    
+    public static void iLoveJava(int n) {
+        int i = 1;
+        while (i <= n) {
+            System.out.println("I love Java!");
+            i++;
+        }
     }
     
-    //11 uses a for loop to count from 1 to 100 by ones, but only values that are evenly divisible by 2 and 9 are printed. (you'll need to use an if statement.)
+    //11 uses a for loop to count from 1 to 100 by ones, but only values that are evenly divisible by 2 and 9 are 
+    //   printed. (you'll need to use an if statement.)
     public static void divBy2and9() {
-    
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0 && i % 9 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }                
     
-    //12 uses a while loop to count from 1 to 100 by ones, but only values that are evenly divisible by 3 and 8 are printed. (you'll need to use an if statement.)
+    //12 uses a while loop to count from 1 to 100 by ones, but only values that are evenly divisible by 3 and 8 
+    //   are printed. (you'll need to use an if statement.)
     public static void divBy3and8() {
-    
+        int i = 1;
+        while (i <= 100) {
+            if (i % 3 == 0 && i % 8 == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+        System.out.println();
     }    
     
-    //13 uses a while loop to find and return the sum of all values in the set {1, 2, 3, ... , 10000} that are divisible by 5 or 7.
+    //13 uses a while loop to find and return the sum of all values in the set {1, 2, 3, ... , 10000} 
+    //   that are divisible by 5 or 7.
     public static void divBy5or7Sum() {
-    
+        int i = 1;
+        int sum = 0;
+        while (i <= 10000) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println(sum);
     }                   
  
-    //14 uses a while loop to count from 1 to n by ones. However, if a number is divisible by 3, the word "bizz" should be printed, and if the number is divisible by 5, "buzz" should be printed. Sample output:
-    public static void bizzBuzz() {
-    
+    //14 uses a while loop to count from 1 to n by ones. However, if a number is divisible by 3, the word "bizz" 
+    //should be printed, and if the number is divisible by 5, "buzz" should be printed. 
+    public static void bizzBuzz(int n) {
+        int i = 1;
+        String name = "";
+        while (i <= n) {
+            if (i % 3 == 0) {
+                name += "Bizz ";
+            }
+            if (i % 5 == 0) {
+                name += "Buzz ";
+            }
+            if (i % 3 !=0 && i % 5 != 0)  {
+                name += (i + " ");
+            }
+            i++;
+        }
+        System.out.println(name);
     }                   
                        
     //15 uses a loop to find and return the sum of the digits in an int.
-    public static void sumDigits() {
-        
-    }    
+    public static int sumDigits(int n) {
+        String strN = n + "";
+        int sum = 0;
+        for (int i = 0; i < strN.length(); i++) {
+            sum += Integer.parseInt(strN.substring(i, i+1));
+        }
+        return sum;
+    }   
     
     //16 uses a loop to count the number of even digits in an int.
-    public static void countEvens() {
-       
+    public static int countEvens(int n) {
+       String strN = n + "";
+       int evens = 0;
+       int digit;
+       for (int i = 0; i < strN.length(); i++) {
+           digit = Integer.parseInt(strN.substring(i, i+1));
+           if (digit % 2 == 0) {
+               evens ++;
+           }
+       }
+       return evens;
     }    
     
     //17 uses the appropriate type of loop to return true if a number n contains the digit d and false otherwise.
-    public static void hasDigit() {
-    
+    public static boolean hasDigit(int n, int d) {
+       String strN = n + "";
+       String strD = d + "";
+       for (int i = 0; i < strN.length(); i++) {
+           if (strN.substring(i, i+1).equals(strD)) {
+               return true;
+           }
+       }
+       return false;
+       /*
+       for (int i = 0; i < (n + "").length(); i++) {
+            if (((n + "").substring(i,i+1)).equals(d+"")) {
+                return true;
+            }
+           
+        }
+        return false;
+        */
     }            
  
     //18 returns true if a number n has more even digits than odd digits and false otherwise.
-    public static void moreEvens() {
-    
+    public static boolean moreEvens() {
+        return false;
     }            
                        
     public static void main(String[] args)
@@ -190,21 +276,19 @@
         twentyToZeroW();
         whereIs5();
         countMtoN(1, 5);
-        */
-        sumN(5);
-        /*
-        perfectSquaresF(5);
-        sumSquaresW();
-        iLoveJava();
+        System.out.println(sumN(3));
+        perfectSquaresF(9);
+        System.out.println(sumSquaresW(9));
+        iLoveJava(3);
         divBy2and9();
         divBy3and8();
         divBy5or7Sum();
-        bizzBuzz();
-        sumDigits();
-        countEvens();
-        hasDigit();
-        moreEvens();
+        bizzBuzz(20);
+        System.out.println(sumDigits(122));
+        System.out.println(countEvens(2212));
+        System.out.println(hasDigit(3, 432));
         */
+        moreEvens();
     }
  }
  
